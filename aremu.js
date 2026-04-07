04bcf13932569d3cdbc2f930bd6db0a150baeb1f
rasheedsalewa <rasheedsalewa11@gmail.com>
2026-04-07 16:04:07 +0100

    make up to github

---
+++ aremu.js
@@ -0,0 +1,88 @@
+// prompt("CAN YOU TELL US YOUR BIGGEST FEAR,,PLEASE?:")
+//VARIABLES
+var aboutpromise = "he is a boy that haves family he cherished so much to the death"
+let aboutfavour="she is a girl that is tall approximately 6ft tall"
+const aboutsalewa="she is of average height the fairest in the ciry of ibadan"
+
+
+
+
+
+/*fgchivdsfatysudhcwqgytdtcuewcygvdgcufdygfdchdsycg
+ftdfgcydfudadfuwvft 
+ ftcudfwujdjfvhijhd*/
+
+// DISPLAY POSSIBILITIES
+// alert
+alert(aboutpromise)
+
+// prompt
+const getage = prompt("how old are you")
+
+
+alert(getage)
+// confirm
+confirm("are you sure you want to close this page")
+// write
+document.write("THIS IS A GIRL")
+
+
+console.log(aboutpromise)
+console.log(aboutsalewa)
+console.log(aboutfavour)
+
+// VAR
+var checkAge= 50
+// console.log(checkAge)
+
+var checkAge= 50
+console.log(checkAge) //redeclaring
+
+// LET
+let checkTime= 1000
+console.log(checkTime)
+   
+ checkTime= 200 //reassigning
+console.log(checkTime) 
+
+
+// CONST
+const myAge= 30
+
+
+  // myAge= 30
+
+  // console.log(myAge)
+
+
+  // DATATYPES
+  // PRIMITIVE DATATYPES
+  // STRING
+
+  let personalities = 'l love being respect so much'
+
+  mypersonalities ="l love being respect so much"
+
+  mypersonalities =`l love being respect so much`
+  
+  console.log(mypersonalities)
+
+  let person = 'SAHEEDAH'
+// string interpolation
+let tellname =`his/her name is ${person}`
+console.log(tellname)
+
+
+let askforintelligence = prompt('ON A SCALE OF 1 TO 10 RATE YOUR INTELLIGENCE?')
+
+let ask = `OK ${askforintelligence},I THINK`
+console.log(ask)
+
+
+let askforname = prompt('what is ur name')
+let askforage = prompt (`how old are you`)
+let greetings = `welcome ${askforname} you are ${askforage}years old`
+console.log(greetings)
+
+
+
